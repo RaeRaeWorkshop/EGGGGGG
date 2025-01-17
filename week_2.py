f10b8eb44af5976eb1e8f99f580bf6f99cedc2e4
Kailyn <jax.cheesecake101@gmail.com>
2025-01-16 19:54:23 -0500

    week2

---
+++ week_2.py
@@ -0,0 +1,42 @@
+#print("ABCD")
+# string1 = "cat"
+# if "cat" == string1:
+#     print("meow")
+
+# ADDing input
+# print("Your name:")
+# name = input()
+# print("hello " + name)
+
+#adding inputs
+# first = input("enter first number: ")
+# second = input("enter second number: ")
+# total = int(first) + int(second) 
+# print("total is " + str(total))
+
+# #while loops
+# i = 1
+# while i < 100:
+#     print(i)
+    #i = i + 1
+    #i += 1
+
+#while name 
+# name = input()
+# while name != "Rae":
+#     name = input()
+
+#printing array
+# friuts = ["apple", "banana", "cherry"]
+# print(friuts[2])
+
+#looping through an array
+# fruits = ["orange", "apple", "banana", "cherry"]
+# # print(fruits[0])
+# # print(fruits[1])
+# # print(fruits[2])
+# i = 0
+# for x in fruits:
+#     if x == "banana":
+#         print(i)
+#     i += 1
