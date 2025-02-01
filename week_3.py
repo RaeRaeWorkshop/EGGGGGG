30ccf423b439acdfbc3c4ed89724d4dcd57bcf55
Kailyn <jax.cheesecake101@gmail.com>
2025-02-01 16:03:24 -0500

    week_3

---
+++ week_3.py
@@ -0,0 +1,38 @@
+# 
+# first = input("enter first number: ")
+# if ("1" not in first) and ("2" not in first) and ("3" not in first) and ("4" not in first) and ("5" not in first) and ("6" not in first) and ("7" not in first) and ("9" not in first) and ("10" not in first):
+#     print("goober")
+# else:
+#     second = input("enter second number: ")
+#     total = int(first) + int(second) 
+#     print("total is " + str(total))
+
+#temp int
+# temp_f = int(input("degrees in F"))
+# temp_c = (temp_f - 32) * 5/9
+# print("temp is " + str(temp_c))
+
+#looping through an array
+# animals = ["dog", "hamster", "Adam", "Neveah", "cats"]
+# for x in animals:
+#     print(x)
+
+# counting = int(input("enter number: "))
+# x = 0
+# while x <= counting:
+#     print(x)
+#     x = x + 1
+
+#generating number
+#import random
+#number = random.random()
+#print(number)
+
+# import random
+# list = [1,2,3,4,5,6,7,8,9,10]
+# number = random.choice(list)
+# print(number)
+
+import random
+number = random.randint(0,100)
+print(number)
